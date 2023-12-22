5ab9b300ccc84c9ee6fdeb139ae9a38da1b0d4ca
Vika Krysheva <v.krysheva@gmail.com>
2023-12-22 17:51:07 +0700

    100 инициатив

--- app.py
+++ app.py
@@ -27,19 +27,19 @@ def load_users(user_id):
     return User.query.get(int(user_id))
 
 
-# def create_sample_initiatives():
-#     with app.app_context():  # Гарантирует, что вы находитесь в контексте приложения Flask
-#         existing_count = Initiative.query.count()
-#         if existing_count < 100:
-#             for i in range(existing_count + 1, 101):  # Creates up to 100 initiatives
-#                 initiative = Initiative(
-#                     title=f"Initiative {i}",
-#                     description=f"Description for Initiative {i}",
-#                     user_id=2  # Предполагается, что 2 — это идентификатор существующего пользователя.
-#                 )
-#                 db.session.add(initiative)
-#             db.session.commit()
-#             #print(f"Added {100 - existing_count} new initiatives.")
+def create_sample_initiatives():
+    with app.app_context():  # Гарантирует, что вы находитесь в контексте приложения Flask
+        existing_count = Initiative.query.count()
+        if existing_count < 100:
+            for i in range(existing_count + 1, 101):  # Creates up to 100 initiatives
+                initiative = Initiative(
+                    title=f"Initiative {i}",
+                    description=f"Description for Initiative {i}",
+                    user_id=2  # Предполагается, что 2 — это идентификатор существующего пользователя.
+                )
+                db.session.add(initiative)
+            db.session.commit()
+            #print(f"Added {100 - existing_count} new initiatives.")
 
-# create_sample_initiatives()
+create_sample_initiatives()
 app.register_blueprint(rgz)
