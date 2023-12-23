e676f096b09a10df7d759db7f86a9abbd822af36
Vika Krysheva <v.krysheva@gmail.com>
2023-12-23 13:01:20 +0700

    error

--- rgz.py
+++ rgz.py
@@ -52,7 +52,7 @@ def admin_users(page=1):
     initiatives = Initiative.query.order_by(Initiative.date_created.desc()).paginate(page=page, per_page=per_page, error_out=False)
     for user in users:
         if user.has_initiative:
-            errors = 'Пользователя с таким именем не существует!'
+            errors = 'Вы не можете удалить пользователя, который имеет инициативу!'
 
     return render_template("admin_users.html", users=users, initiatives = initiatives, errors=errors)
 
