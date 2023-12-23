3bd8d57bd5ec4e0fc3cc1d23426967b04ce7200e
Vika Krysheva <v.krysheva@gmail.com>
2023-12-23 12:58:41 +0700

    error

--- rgz.py
+++ rgz.py
@@ -42,6 +42,7 @@ def home(page=1):
 @rgz.route('/admin/users/<int:page>')
 @login_required
 def admin_users(page=1):
+    errors = ''
     if not current_user.is_authenticated or not current_user.is_admin:
         return "Access Denied", 403
 
@@ -49,8 +50,11 @@ def admin_users(page=1):
     per_page = 20
     
     initiatives = Initiative.query.order_by(Initiative.date_created.desc()).paginate(page=page, per_page=per_page, error_out=False)
+    for user in users:
+        if user.has_initiative:
+            errors = 'Пользователя с таким именем не существует!'
 
-    return render_template("admin_users.html", users=users, initiatives = initiatives)
+    return render_template("admin_users.html", users=users, initiatives = initiatives, errors=errors)
 
 
 @rgz.route("/home/register", methods=['GET', 'POST'])
